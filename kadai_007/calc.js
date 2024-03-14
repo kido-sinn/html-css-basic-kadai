861bb3d8700e5409a3b6545619aa7a225b92563e
kido-sinn <157770540+kido-sinn@users.noreply.github.com>
2024-03-14 19:07:46 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,10 @@
-let num = Math.floor(Math.random() *16);
+let num = Math.floor(Math.random()*30);
 
 console.log(num);
 
-if(num % 15 ===0) {
+if(num % 15 === 0) {
   console.log('3と5の倍数です');
 }
-
 
 else if(num % 3 === 0) {
   console.log('3の倍数です');
@@ -14,4 +13,6 @@ else if(num % 3 === 0) {
 else if(num % 5 === 0) {
   console.log('5の倍数です');
 }
-
+else {
+   console.log(num);
+ }
