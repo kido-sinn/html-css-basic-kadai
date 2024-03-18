8d5f898cd828e6bac9cadd56e6f85ffcabcf7868
kido-sinn <157770540+kido-sinn@users.noreply.github.com>
2024-03-18 14:26:24 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const date = new Date();
+const [year, month, day] = [
+  date.getFullYear(2024),
+  date.getMonth(3) + 1,
+  date.getDate(18),
+]
+console.log(year + '年' +　month + '月' + day + '日');
