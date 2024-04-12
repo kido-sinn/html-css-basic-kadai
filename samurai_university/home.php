60ac62c8a62ed6223f77403bf3268937f4970b66
kido-sinn <157770540+kido-sinn@users.noreply.github.com>
2024-04-12 13:37:27 +0900

    Add files via upload

--- samurai_university/home.php
+++ samurai_university/home.php
@@ -11,9 +11,9 @@
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                 <img src="<?php echo get_template_directory_uri(); ?>/images/top1_1200_630.jpg" class="d-block w-100" style="background-color: #2b7b8e26" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top1_1200_630.jpg" class="d-block w-100" style="background-color: #2b7b8e26" />
                 <div class="carousel-caption d-none d-md-block">
-                   <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big_slide.png" />
+                  <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big_slide.png" />
                   <div class="title-slide">
                     <p>Samurai University</p>
                   </div>
@@ -30,7 +30,7 @@
               <div class="carousel-item">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/top2_1200_630.jpg" class="d-block w-100" />
                 <div class="carousel-caption d-none d-md-block">
-                   <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big_slide.png" />
+                  <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big_slide.png" />
                   <div class="title-slide">
                     <p>Samurai University</p>
                   </div>
@@ -45,9 +45,9 @@
                 </div>
               </div>
               <div class="carousel-item">
-                 <img src="<?php echo get_template_directory_uri(); ?>/images/top3_1200_630.jpg" class="d-block w-100" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/top3_1200_630.jpg" class="d-block w-100" />
                 <div class="carousel-caption d-none d-md-block">
-                   <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big_slide.png" />
+                  <img src="<?php echo get_template_directory_uri(); ?>/images/logo_big_slide.png" />
                   <div class="title-slide">
                     <p>Samurai University</p>
                   </div>
@@ -77,11 +77,10 @@
     <!-- メインビジュアルここまで -->
 
     <!-- ニュースとイベント -->
-    <div class="news">
-      <div class="footer_row">
-        <div class="row news_row">
-          <!-- ニュース -->
-          <div class="col-lg-6 col-md-6 col-sx-12 news_col">
+   
+   
+ <!-- ニュース -->
+ <div class="col-lg-6 col-md-6 col-sx-12 news_col">
             <div class="home_title">News</div>
             <div class="home_title_sub">ニュース</div>
             <?php
@@ -96,82 +95,91 @@
                );
                // データの取得
                $posts = get_posts($args);
-               // ループ処理
-               foreach($posts as $post):
-                 // 投稿データをセット
-                 setup_postdata($post);
-                 // パーマリンクを表示
-                 echo get_permalink();
-                 echo "<br/>";
-                 // 投稿の日付を表示
-                 echo get_the_date();
-                 echo "<br/>";
-                 // 投稿のタイトルを表示
-                 the_title();
-                 echo "<br/><br/>";
-               endforeach;
-               // 使用した投稿データをリセット
-               wp_reset_postdata();
              ?>
+ 
+             <!-- ループ処理 -->
+             <?php foreach($posts as $post): ?>
+             <?php setup_postdata($post); ?>
+             <div class="news_post_small">
+               <div class="news_post_meta">
+                 <ul>
+                   <li>
+                     <!-- aタグで投稿記事へのリンクを作成 -->
+                     <a href="<?php echo get_permalink(); ?>">
+                       <!-- 日付を出力する -->
+                       <?php echo get_the_date(); ?>
+                     </a>
+                   </li>
+                 </ul>
+               </div>
+               <div class="news_post_small_title">
+                 <!--  aタグで投稿記事へのリンクを作成する -->
+                 <a href="<?php the_permalink(); ?>">
+                   <!--  投稿記事のタイトルを表示する -->
+                   <?php the_title(); ?>
+                 </a>
+               </div>
+             </div>
+ 
+             <?php endforeach; ?>
+             <!-- 使用した投稿データをリセット -->
+             <?php wp_reset_postdata(); ?>
           </div>
           <!-- ニュースここまで -->
-
-          <!-- イベント -->
-          <div class="col-lg-6 col-md-6 col-sx-12 news_col">
+       <!-- イベント -->
+       <div class="col-lg-6 col-md-6 col-sx-12 news_col">
             <div class="home_title">Event</div>
             <div class="home_title_sub">イベント</div>
-            <div class="news_post_small">
-              <div class="row news_post_row">
-                <div class="col-lg-3 col-md-4 col-sx-12">
-                  <div class="calendar_border">
-                    <div class="calendar_border_1">
-                      <div class="calendar_month">OCT</div>
-                      <div class="calendar_day">
-                        <span>8</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-9 col-md-8 col-sx-12">
-                  <div class="news_post_small_title">
-                    <a href="events_detail.html">AWSハンズオンセミナー</a>
-                  </div>
-                  <div class="news_post_content">
-                    10月8日　新宿〇〇ビル8Fにて、AWSハンズオンセミナーを開催します。AWSを実際に構築してみます。参加希望者は...
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="news_post_small">
-              <div class="row news_post_row">
-                <div class="col-lg-3 col-md-4 col-sx-12">
-                  <div class="calendar_border">
-                    <div class="calendar_border_1">
-                      <div class="calendar_month">OCT</div>
-                      <div class="calendar_day">
-                        <span>15</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-9 col-md-8 col-sx-12">
-                  <div class="news_post_small_title">
-                    <a href="events_detail.html">Raspberry Piハンズオンセミナー</a>
-                  </div>
-                  <div class="news_post_content">
-                    11月15日　新宿〇〇ビル8Fにて、Paspberry
-                    Piｗ使った、センサーを利用した自動走行ハンズオンセミナーを開催し...
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            <?php
+               //取得したい投稿記事などの条件を引数として渡す
+               $args = array(
+                   // 投稿タイプ
+                   'post_type'      => 'post',
+                   // カテゴリー名
+                   'category_name' => 'event',
+                   // 1ページに表示する投稿数
+                   'posts_per_page' => 2,
+               );
+               // データの取得
+               $posts = get_posts($args);
+             ?>
+ 
+             <!-- ループ処理 -->
+             <?php foreach($posts as $post): ?>
+               <?php setup_postdata($post); ?>
+               <div class="news_post_small">
+                 <div class="row news_post_row">
+                   <div class="col-lg-3 col-md-4 col-sx-12">
+                     <div class="calendar_border">
+                       <div class="calendar_border_1">
+                         <div class="calendar_month">OCT</div>
+                         <div class="calendar_day">
+                           <span>8</span>
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                   <div class="col-lg-9 col-md-8 col-sx-12">
+                     <div class="news_post_small_title">
+                       <a href="<?php the_permalink(); ?>">
+                         <?php the_title() ; ?>
+                       </a>
+                     </div>
+                     <div class="news_post_content">
+                       <?php 
+                         $content = wp_trim_words( get_the_content() ,  50, '...');
+                         echo $content;
+                       ?>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             <?php endforeach; ?>
+             <!-- 使用した投稿データをリセット -->
+             <?php wp_reset_postdata(); ?>
+             </div>
           <!-- イベントここまで -->
-        </div>
-      </div>
-    </div>
-    <!-- ニュースとイベントここまで -->
-
+          <!-- ニュースとイベントここまで -->
     <!-- コース -->
     <div class="courses">
       <div class="footer_row">
@@ -187,7 +195,7 @@
           <div class="col-lg-4 course_col">
             <div class="course">
               <div class="course_image">
-                 <img src="<?php echo get_template_directory_uri(); ?>/images/pic6-3.jpg" alt="" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/pic6-3.jpg" alt="" />
               </div>
               <div class="course_body">
                 <h3 class="course_title">
@@ -209,7 +217,7 @@
           <div class="col-lg-4 course_col">
             <div class="course">
               <div class="course_image">
-                 <img src="<?php echo get_template_directory_uri(); ?>/images/pic6-1.jpg" alt="" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/pic6-1.jpg" alt="" />
               </div>
               <div class="course_body">
                 <h3 class="course_title">
@@ -231,7 +239,7 @@
           <div class="col-lg-4 course_col">
             <div class="course">
               <div class="course_image">
-                 <img src="<?php echo get_template_directory_uri(); ?>/images/pic6-4.jpg" alt="" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/pic6-4.jpg" alt="" />
               </div>
               <div class="course_body">
                 <h3 class="course_title"><a href="courses_detail.html">法学部</a></h3>
@@ -249,11 +257,12 @@
         </div>
       </div>
     </div>
+     
     <!-- コースここまで -->
 
     <!-- 数字 -->
     <div class="counter">
-       <div class="counter_background" style="background-image: url(<?php echo get_template_directory_uri();?>/images/pic_chart.png)"></div>
+      <div class="counter_background" style="background-image: url(<?php echo get_template_directory_uri();?>/images/pic_chart.png)"></div>
       <div class="footer_row">
         <div class="">
           <div class="">
@@ -262,28 +271,28 @@
                 <div class="milestone col-md-3 col-lg-3 col-sx-12">
                   <div class="milestone_counter">
                     <p>教授数</p>
-                     <img src="<?php echo get_template_directory_uri(); ?>/images/icon4.png" alt="" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon4.png" alt="" />
                     <span class="milestone_number">335</span><span class="milestone_count">名</span>
                   </div>
                 </div>
                 <div class="milestone col-md-3 col-lg-3 col-sx-12">
                   <div class="milestone_counter">
                     <p>卒業生徒</p>
-                     <img src="<?php echo get_template_directory_uri(); ?>/images/icon3.png" alt="" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon3.png" alt="" />
                     <span class="milestone_number">4526</span><span class="milestone_count">名</span>
                   </div>
                 </div>
                 <div class="milestone col-md-3 col-lg-3 col-sx-12">
                   <div class="milestone_counter">
                     <p>学習コース</p>
-                     <img src="<?php echo get_template_directory_uri(); ?>/images/icon2.png" alt="" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon2.png" alt="" />
                     <span class="milestone_number">12</span><span class="milestone_count">コース</span>
                   </div>
                 </div>
                 <div class="milestone col-md-3 col-lg-3 col-sx-12">
                   <div class="milestone_counter">
                     <p>受賞</p>
-                     <img src="<?php echo get_template_directory_uri(); ?>/images/icon1.png" alt="" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon1.png" alt="" />
                     <span class="milestone_number">35</span><span class="milestone_count">回</span>
                   </div>
                 </div>
